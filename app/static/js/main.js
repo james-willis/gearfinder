a85715ff2fce5c7fd09c4f7a101ab59bee814325
James <jameswillis@u.northwestern.edu>
2017-04-07 14:35:05 -0600

    fixed js parens

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,15 +1,18 @@
-
 $(document).ready(function(){
-    $("#searchButton").click(function(){
-        // remove old warning box
-        $(".alert").remove()
-
-        // get search terms from form and request the results
-    	var search_terms = $("#searchForm").serializeArray()[1]["value"];	
-    	$("#results").html('');
-    	load_posts(search_terms);
-    });
+    $("#searchButton").click(submitSearch);
 });
+
+function submitSearch(){
+    // remove old warning box
+    $(".alert").remove();
+
+    // empty the current results
+    $("#results").html('');
+
+    // get search terms from form and request the results
+    var search_terms = $("#searchForm").serializeArray()[1].value;
+    load_posts(search_terms);
+}
 
 function load_posts(terms) {
     // build url and add loading message
@@ -18,9 +21,9 @@ function load_posts(terms) {
 
     // request results and add them to the DOM
 	$("#results").load(url, searchCallback);
-};
+}
 
 function searchCallback(result){
 	// remove loading message
-    $("#loadMarker").remove()
-};
+    $("#loadMarker").remove();
+}
